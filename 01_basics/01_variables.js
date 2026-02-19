e750a6a39232224b8344d72bb32f12d98ce10da6
abhi-shaji <109214876+abhi-shaji@users.noreply.github.com>
2026-02-19 09:04:30 +0000

    variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountEmail = "abhinavshaji16@gmail.com" 
+let accountPassword = "12345"
+var accountCity = "Bangalore" //Avoid Var for declaration
+let accountState; // Avid this declaration
+
+// Comment sample
+accountPassword = "123456789" // Re-assigning the value of accountPassword
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTpes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as new version
+
+// alert(3 + 4); // We are using node js not browser, so alert will not work.
+
+//number
+//bigint
+//string
+//null => standalon value
+//undefined => no value is assigned to variable
+//symbol => unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
