0828c3da08f36e924aac5b03579feb14ca8ab26b
abhi-shaji <109214876+abhi-shaji@users.noreply.github.com>
2026-02-21 04:46:03 +0000

    conversio of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score = "";
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+
+//string to number => NaN
+//string number to number possible
+//true to number => 1
+//undefined to number => Nan
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true, 0=> false
+//""=>false, "AAA" =>true
+
