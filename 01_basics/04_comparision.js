191c278a25eadcf6825d62755fd5ec660cd9e141
abhi-shaji <109214876+abhi-shaji@users.noreply.github.com>
2026-02-24 08:03:47 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==2);
+// console.log(2=="2");
+// console.log(2===2);
+// console.log(2==="2");
+
+console.log(undefined >= 0);
+console.log(null >= 0);
+console.log(NaN >= 0);
+
+//ALWAYS COMPARE VALUES WITH SAME DATA TYPES
