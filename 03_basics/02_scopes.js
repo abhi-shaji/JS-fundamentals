c7fa24a5c03224b1c8240b7b11d72805ab4b592b
abhi-shaji <109214876+abhi-shaji@users.noreply.github.com>
2026-03-03 14:14:22 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,30 @@
+// var c = 300
+
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log(`inside block a value: ${a}`);
+    
+}
+
+// console.log(a);
+// console.log(b);
+
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+function one () {
+    const username = "Abhinav"
+    function two () {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+one()
